66df32a78a58f03b9b64bfdc5c898809f14b0d8e
Henrique Marlon <henriquemarloncs@gmail.com>
2025-02-07 09:06:03 +0200

    feat: scripts and makefile

---
+++ contracts/script/Deploy.s.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import {Token} from "../src/Token.sol";
+import {TreeDetector} from "../src/TreeDetector.sol";
+import {Script} from "../lib/forge-std/src/Script.sol";
+
+contract Deploy is Script {
+    function run() external returns (Token, TreeDetector) {
+        // These values should be replaced with your actual values
+        address taskIssuerAddress = vm.envAddress("TASK_ISSUER_ADDRESS");
+        bytes32 machineHash = vm.envBytes32("MACHINE_HASH");
+
+        vm.startBroadcast();
+        Token token = new Token();
+        TreeDetector treeDetector = new TreeDetector(taskIssuerAddress, machineHash);
+        vm.stopBroadcast();
+
+        return (token, treeDetector);
+    }
+}
